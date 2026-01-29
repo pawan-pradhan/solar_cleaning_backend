11d9132d8d5f2ce74c2f9c99045fd61cfbfe75be
pawan-pradhan <pawanpradhantng@gmail.com>
2026-01-30 03:50:00 +0530

    set false cookie secure

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -118,7 +118,7 @@ const loginCheck = async (req, res) => {
             maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
             httpOnly: true,
             signed: true,
-            secure: process.env.NODE_ENV === 'production',
+            secure: false,
             sameSite: 'lax'
         });
 
